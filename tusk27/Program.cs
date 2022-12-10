a4b4696a84e82bf9b89a72ec7674391bde384bc9
Evgeni.ok14 <evgeni.ok@mail.ru>
2022-12-10 12:22:04 +0300

    added a new project: tusk27

---
+++ tusk27/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string s = n.ToString();
+int Sum = 0;
+for (int i = 0; i < s.Length; i++)
+{ 
+   int d = Convert.ToInt32(s[i].ToString());
+   Sum += d;
+}
+Console.WriteLine("Sum = " + Sum); 
