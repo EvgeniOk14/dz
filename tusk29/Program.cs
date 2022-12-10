86ee97cb3d4db065f26c9f4bc883506a2a518605
Evgeni.ok14 <evgeni.ok@mail.ru>
2022-12-10 16:45:15 +0300

    added a new project: tusk29

---
+++ tusk29/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Задайте размер массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int [] array = new int[n];
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"Введите элемент массива под индексом { i }  {":"} ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}   
+  Console.WriteLine("\n Вывод массива: \n");
+  Console.Write($"[{string.Join(", ", array)}]");
+
