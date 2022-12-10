7f24415c5cb5e07ee8b271077b43c2d5e806a710
Evgeni.ok14 <evgeni.ok@mail.ru>
2022-12-10 11:52:35 +0300

    added a new project: tusk25

---
+++ tusk25/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите число А: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+int result = A;;
+ for (int i = 2; i <= B; i++)
+ {
+    result = result * A;
+ }
+Console.WriteLine(result);
